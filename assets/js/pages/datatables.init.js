7447c300accd9d2414c3dd55522d97c038817578
Abdulqodir <rasuldjanovich7@gmail.com>
2023-06-15 17:05:14 +0300

    first commit

--- assets/js/pages/datatables.init.js
+++ assets/js/pages/datatables.init.js
@@ -1,1 +1,14 @@
-$(document).ready(function(){$("#datatable").DataTable(),$("#datatable-buttons").DataTable({lengthChange:!1,buttons:["copy","excel","pdf","colvis"]}).buttons().container().appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)"),$(".dataTables_length select").addClass("form-select form-select-sm")});
+$(document).ready(function () {
+    dTable = $("#datatable").DataTable({
+        "dom": 'rtip'
+        //paging: true,
+        //info: false
+    }), $("#datatable-buttons").DataTable({
+        lengthChange: !1,
+        buttons: ["copy", "excel", "pdf", "colvis"]
+    }).buttons().container().appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)"), $(".dataTables_length select").addClass("form-select form-select-sm")
+    
+    $('#cSearch').keyup(function () {
+        dTable.search($(this).val()).draw();   // this  is for customized searchbox with datatable search feature.
+    })
+});
